1cb51f7c375b6ca50d1fb83931406bb7cfeff0ce
Rohit sharma <rsharma07feb@gmail.com>
2024-04-01 02:52:37 +0530

    auth setup

---
+++ src/appwrite/auth.js
@@ -0,0 +1,51 @@
+import conf from "../conf/conf";
+import { Client, Account } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async creatAccount({ email, password, name }) {
+    try {
+      await this.Account.creat(ID.uniqe(), email, password, name);
+      if (userAccount) {
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+    }
+
+    return null;
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error", error);
+    }
+  }
+}
+const authsevice = new AuthService();
+export default authsevice;
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,8 +1,8 @@
 const conf = {
-  appwiteUrl: string(import.meta.env.VITE_APPWRITE_URL),
-  appwiteProjectId: string(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-  appwiteDatabaseId: string(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  appwiteCollectionId: string(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-  appwiteBucketId: string(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  appwriteUrl: string(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: string(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: string(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: string(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: string(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 };
 export default conf;
